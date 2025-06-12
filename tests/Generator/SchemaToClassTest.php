4751a95c4db0918be3c462d9e4c933e81b4090fa
alexchexes <alexchexes@gmail.com>
2025-06-13 00:11:18 +0100

    test: add tests for schemas with multiple definitions

--- tests/Generator/SchemaToClassTest.php
+++ tests/Generator/SchemaToClassTest.php
@@ -5,6 +5,9 @@ namespace Helmich\Schema2Class\Generator;
 
 use Helmich\Schema2Class\Example\CustomerAddress;
 use Helmich\Schema2Class\Loader\SchemaLoader;
+use Helmich\Schema2Class\Command\GenerateSpecCommand;
+use Helmich\Schema2Class\Generator\Property\IntersectProperty;
+use Helmich\Schema2Class\Generator\Property\NestedObjectProperty;
 use Helmich\Schema2Class\Spec\SpecificationOptions;
 use Helmich\Schema2Class\Spec\ValidatedSpecificationFilesItem;
 use Helmich\Schema2Class\Writer\DebugWriter;
@@ -80,17 +83,23 @@ class SchemaToClassTest extends TestCase
             $opts,
         );
 
-        $req = $req->withReferenceLookup(new class ($schema) implements ReferenceLookup {
-            public function __construct(private readonly array $schema)
-            {
-            }
+        $definitions = $schema['definitions'] ?? [];
+        $definitionsLookup = new DefinitionsReferenceLookup($definitions);
+
+        $req = $req->withReferenceLookup(new class ($definitionsLookup) implements ReferenceLookup {
+            public function __construct(private DefinitionsReferenceLookup $lookup) {}
 
             public function lookupReference(string $reference): ReferencedType
             {
                 if ($reference === "#/properties/address") {
                     return new ReferencedTypeClass(CustomerAddress::class);
                 }
-                return new ReferencedTypeUnknown();
+
+                $result = $this->lookup->lookupReference($reference);
+                if ($result instanceof ReferencedTypeUnknown) {
+                    return new ReferencedTypeUnknown();
+                }
+                return $result;
             }
 
             public function lookupSchema(string $reference): array
@@ -112,24 +121,86 @@ class SchemaToClassTest extends TestCase
                         ],
                     ];
                 }
-                return [];
+
+                return $this->lookup->lookupSchema($reference);
             }
         });
 
-        $output = new NullOutput();
-        $writer = new DebugWriter($output);
+        $output   = new NullOutput();
+        $writer   = new DebugWriter($output);
+        $factory  = new SchemaToClassFactory();
 
-        (new SchemaToClassFactory())->build($writer, $output)->schemaToClass($req);
+        $collectAllLocalRefs = static function (array $schema, array $allDefinitions) {
+            $needed  = [];
+            $queue   = [$schema];
+            $visited = [];
 
-        $this->assertCount(
-            expectedCount: count($expectedOutput),
-            haystack: $writer->getWrittenFiles(),
-            message: sprintf(
-                'Expected file count [%s] does not match the written file count [%s]',
-                implode(', ', array_keys($expectedOutput)),
-                implode(', ', array_keys($writer->getWrittenFiles())),
-            ),
-        );
+            while ($cur = array_pop($queue)) {
+                $iter = function ($node) use (&$iter, &$needed, &$queue, &$visited, $allDefinitions) {
+                    if (is_array($node)) {
+                        foreach ($node as $k => $v) {
+                            if ($k === '$ref'
+                                && is_string($v)
+                                && str_starts_with($v, '#/definitions/')) {
+                                $name = substr($v, 14);
+                                if (!isset($visited[$name])) {
+                                    $visited[$name] = true;
+                                    $needed[]       = $name;
+
+                                    if (isset($allDefinitions[$name])) {
+                                        $queue[] = $allDefinitions[$name];
+                                    }
+                                }
+                            } elseif (is_array($v)) {
+                                $iter($v);
+                            }
+                        }
+                    }
+                };
+                $iter($cur);
+            }
+
+            return $needed;
+        };
+
+        $generatedClasses = array_keys($definitions);
+        $generatedClasses[] = $req->getTargetClass();
+
+        foreach ($definitions as $defName => $defSchema) {
+            $deps        = $collectAllLocalRefs($defSchema, $definitions);
+            $trimmedDefs = array_intersect_key($definitions, array_flip($deps));
+
+            $defReq = $req
+                ->withClass($defName)
+                ->withSchema($defSchema)
+                ->withRootDefinitions($trimmedDefs)
+                ->withGeneratedClassNames($generatedClasses);
+
+            $factory->build($writer, $output)->schemaToClass($defReq);
+        }
+
+        $req = $req
+            ->withRootDefinitions($definitions)
+            ->withGeneratedClassNames($generatedClasses);
+
+        if (NestedObjectProperty::canHandleSchema($schema)
+            || IntersectProperty::canHandleSchema($schema)
+            || isset($schema['enum'])
+        ) {
+            $factory->build($writer, $output)->schemaToClass($req);
+        }
+
+        if (getenv('UPDATE_SNAPSHOTS') !== '1') {
+            $this->assertCount(
+                expectedCount: count($expectedOutput),
+                haystack: $writer->getWrittenFiles(),
+                message: sprintf(
+                    'Expected file count [%s] does not match the written file count [%s]',
+                    implode(', ', array_keys($expectedOutput)),
+                    implode(', ', array_keys($writer->getWrittenFiles())),
+                ),
+            );
+        }
         foreach ($expectedOutput as $file => $content) {
             $filename      = __DIR__ . '/' . $file;
             $actualContent = $writer->getWrittenFiles()[$filename];
@@ -140,6 +211,10 @@ class SchemaToClassTest extends TestCase
             } else {
                 assertThat($actualContent, equalTo($content));
             }
+        }
+
+        if (getenv('UPDATE_SNAPSHOTS') === '1') {
+            $this->addToAssertionCount(1);
         }
     }
 }
