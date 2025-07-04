fef2aed13782bb1b8e9da609abae8c472bf9a7af
alexchexes <alexchexes@gmail.com>
2025-07-04 14:50:42 +0100

    add failing test fixture

---
+++ tests/Generator/Fixtures/PropertyRefDefInObject/Output/OptionsObject.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ns\PropertyRefDefInObject;
+
+class OptionsObject
+{
+    /**
+     * Schema used to validate input for creating instances of this class
+     *
+     * @var array
+     */
+    private static array $schema = [
+        'properties' => [
+            'output' => [
+                'type' => 'string',
+            ],
+        ],
+    ];
+
+    /**
+     * @var string|null
+     */
+    private ?string $output = null;
+
+    /**
+     * @return string|null
+     */
+    public function getOutput() : ?string
+    {
+        return $this->output ?? null;
+    }
+
+    /**
+     * @param string $output
+     * @return self
+     * @param bool $validate
+     */
+    public function withOutput(string $output, bool $validate = true) : self
+    {
+        if ($validate) {
+            $validator = new \JsonSchema\Validator();
+            $validator->validate($output, self::$schema['properties']['output']);
+            if (!$validator->isValid()) {
+                throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+            }
+        }
+
+        $clone = clone $this;
+        $clone->output = $output;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutOutput() : self
+    {
+        $clone = clone $this;
+        unset($clone->output);
+
+        return $clone;
+    }
+
+    /**
+     * Builds a new instance from an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $validate Set this to false to skip validation; use at own risk
+     * @return OptionsObject Created instance
+     * @throws \InvalidArgumentException
+     */
+    public static function buildFromInput(array|object $input, bool $validate = true) : OptionsObject
+    {
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        if ($validate) {
+            static::validateInput($input);
+        }
+
+        $output = isset($input->{'output'}) ? $input->{'output'} : null;
+
+        $obj = new self();
+        $obj->output = $output;
+        return $obj;
+    }
+
+    /**
+     * Converts this object back to a simple array that can be JSON-serialized
+     *
+     * @return array Converted array
+     */
+    public function toArray() : array
+    {
+        $output = [];
+        if (isset($this->output)) {
+            $output['output'] = $this->output;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Validates an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result
+     * @throws \InvalidArgumentException
+     */
+    public static function validateInput(array|object $input, bool $return = false) : bool
+    {
+        $validator = new \JsonSchema\Validator();
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        $validator->validate($input, self::$schema);
+
+        if (!$validator->isValid() && !$return) {
+            $errors = array_map(function(array $e): string {
+                return ($e["property"] ? $e["property"] . ": " : "") . $e["message"];
+            }, $validator->getErrors());
+            throw new \InvalidArgumentException(join(".\n", $errors));
+        }
+
+        return $validator->isValid();
+    }
+}
