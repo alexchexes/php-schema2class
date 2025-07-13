7801409e424886ecaca9020f39831c6168c39fa8
alexchexes <alexchexes@gmail.com>
2025-07-13 16:47:39 +0100

    add test

---
+++ tests/Generator/Fixtures/EnumUnsupported/Output-5.6/Foo.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Ns\EnumUnsupported_5_6;
+
+class Foo
+{
+    /**
+     * Schema used to validate input for creating instances of this class
+     *
+     * @var array
+     */
+    private static $schema = [
+        'type' => 'object',
+        'properties' => [
+            'foo' => [
+                'enum' => [
+                    0,
+                    1.5,
+                    2.5,
+                    3.5,
+                ],
+                'type' => 'number',
+            ],
+            'bar' => [
+                '$ref' => '#/definitions/Bar',
+            ],
+        ],
+        'definitions' => [
+            'Bar' => [
+                'type' => 'number',
+                'enum' => [
+                    0,
+                    1.5,
+                    2.5,
+                    3.5,
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * @var int|float|null
+     */
+    private $foo = null;
+
+    /**
+     * @var 0|1|2|3|null
+     */
+    private $bar = null;
+
+    /**
+     * @return int|float|null
+     */
+    public function getFoo()
+    {
+        return $this->foo;
+    }
+
+    /**
+     * @return 0|1|2|3|null
+     */
+    public function getBar()
+    {
+        return $this->bar;
+    }
+
+    /**
+     * @param int|float $foo
+     * @return self
+     * @param bool $validate
+     */
+    public function withFoo($foo, bool $validate = true)
+    {
+        if ($validate) {
+            $validator = new \JsonSchema\Validator();
+            $validator->validate($foo, self::$schema['properties']['foo']);
+            if (!$validator->isValid()) {
+                throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+            }
+        }
+
+        $clone = clone $this;
+        $clone->foo = $foo;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutFoo()
+    {
+        $clone = clone $this;
+        unset($clone->foo);
+
+        return $clone;
+    }
+
+    /**
+     * @param 0|1|2|3 $bar
+     * @return self
+     * @param bool $validate
+     */
+    public function withBar(int $bar, bool $validate = true)
+    {
+        if ($validate) {
+            $validator = new \JsonSchema\Validator();
+            $validator->validate($bar, self::$schema['properties']['bar']);
+            if (!$validator->isValid()) {
+                throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+            }
+        }
+
+        $clone = clone $this;
+        $clone->bar = $bar;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutBar()
+    {
+        $clone = clone $this;
+        unset($clone->bar);
+
+        return $clone;
+    }
+
+    /**
+     * Builds a new instance from an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $validate Set this to false to skip validation; use at own risk
+     * @return Foo Created instance
+     * @throws \InvalidArgumentException
+     */
+    public static function buildFromInput($input, bool $validate = true)
+    {
+        if (!is_array($input) && !is_object($input)) {
+            throw new \InvalidArgumentException(
+                'Input to buildFromInput must be array or object, got ' . gettype($input)
+            );
+        }
+
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        if ($validate) {
+            static::validateInput($input);
+        }
+
+        $foo = isset($input->{'foo'}) ? $input->{'foo'} : null;
+        $bar = isset($input->{'bar'}) ? $input->{'bar'} : null;
+
+        $obj = new self();
+        $obj->foo = $foo;
+        $obj->bar = $bar;
+        return $obj;
+    }
+
+    /**
+     * Converts this object back to a simple array that can be JSON-serialized
+     *
+     * @return array Converted array
+     */
+    public function toArray()
+    {
+        $output = [];
+        if (isset($this->foo)) {
+            $output['foo'] = $this->foo;
+        }
+        if (isset($this->bar)) {
+            $output['bar'] = $this->bar;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Validates an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result
+     * @throws \InvalidArgumentException
+     */
+    public static function validateInput($input, $return = false)
+    {
+        $validator = new \JsonSchema\Validator();
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        $validator->validate($input, self::$schema);
+
+        if (!$validator->isValid() && !$return) {
+            $errors = array_map(function($e) {
+                return ($e["property"] ? $e["property"] . ": " : "") . $e["message"];
+            }, $validator->getErrors());
+            throw new \InvalidArgumentException(join(".\n", $errors));
+        }
+
+        return $validator->isValid();
+    }
+}
