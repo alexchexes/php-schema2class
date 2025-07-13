48acfcebab30434346656713ff80ff1d14baf333
alexchexes <alexchexes@gmail.com>
2025-07-13 10:34:55 +0100

    add test case

---
+++ tests/Generator/Fixtures/RefSetterValidation/Output-8.4/Cat.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ns\RefSetterValidation;
+
+class Cat
+{
+    /**
+     * Schema used to validate input for creating instances of this class
+     *
+     * @var array
+     */
+    private static array $schema = [
+        'type' => 'object',
+        'properties' => [
+            'hasFur' => [
+                '$ref' => '#/definitions/furBoolean',
+            ],
+        ],
+        'definitions' => [
+            'furBoolean' => [
+                'type' => [
+                    'boolean',
+                    'null',
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * @var bool|null
+     */
+    private ?bool $hasFur = null;
+
+    /**
+     * @return bool|null
+     */
+    public function getHasFur() : ?bool
+    {
+        return $this->hasFur ?? null;
+    }
+
+    /**
+     * @param bool $hasFur
+     * @return self
+     * @param bool $validate
+     */
+    public function withHasFur(bool $hasFur, bool $validate = true) : self
+    {
+        if ($validate) {
+            $validator = new \JsonSchema\Validator();
+            $validator->validate($hasFur, self::$schema['properties']['hasFur']);
+            if (!$validator->isValid()) {
+                throw new \InvalidArgumentException($validator->getErrors()[0]['message']);
+            }
+        }
+
+        $clone = clone $this;
+        $clone->hasFur = $hasFur;
+
+        return $clone;
+    }
+
+    /**
+     * @return self
+     */
+    public function withoutHasFur() : self
+    {
+        $clone = clone $this;
+        unset($clone->hasFur);
+
+        return $clone;
+    }
+
+    /**
+     * Builds a new instance from an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $validate Set this to false to skip validation; use at own risk
+     * @return Cat Created instance
+     * @throws \InvalidArgumentException
+     */
+    public static function buildFromInput(array|object $input, bool $validate = true) : Cat
+    {
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        if ($validate) {
+            static::validateInput($input);
+        }
+
+        $hasFur = isset($input->{'hasFur'}) ? $input->{'hasFur'} : null;
+
+        $obj = new self();
+        $obj->hasFur = $hasFur;
+        return $obj;
+    }
+
+    /**
+     * Converts this object back to a simple array that can be JSON-serialized
+     *
+     * @return array Converted array
+     */
+    public function toArray() : array
+    {
+        $output = [];
+        $output['hasFur'] = $this->hasFur;
+
+        return $output;
+    }
+
+    /**
+     * Validates an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result
+     * @throws \InvalidArgumentException
+     */
+    public static function validateInput(array|object $input, bool $return = false) : bool
+    {
+        $validator = new \JsonSchema\Validator();
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        $validator->validate($input, self::$schema);
+
+        if (!$validator->isValid() && !$return) {
+            $errors = array_map(function(array $e): string {
+                return ($e["property"] ? $e["property"] . ": " : "") . $e["message"];
+            }, $validator->getErrors());
+            throw new \InvalidArgumentException(join(".\n", $errors));
+        }
+
+        return $validator->isValid();
+    }
+}
