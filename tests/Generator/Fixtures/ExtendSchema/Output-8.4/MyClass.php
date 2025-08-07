fdbec54b19e850b488bd083c611651958a890ef0
alexchexes <alexchexes@gmail.com>
2025-08-07 21:55:15 +0100

    add test case

---
+++ tests/Generator/Fixtures/ExtendSchema/Output-8.4/MyClass.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ns\ExtendSchema_8_4;
+
+class MyClass
+{
+    /**
+     * Schema used to validate input for creating instances of this class
+     *
+     * @var array
+     */
+    private static array $_schema = [
+        'required' => [
+            
+        ],
+        'properties' => [
+            'state' => [
+                'type' => 'string',
+            ],
+        ],
+    ];
+
+    private ?string $state = null;
+
+    public function __construct(?string $state = null)
+    {
+        $this->state = $state;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function withState(string $state): self
+    {
+        $clone = clone $this;
+        $clone->state = $state;
+
+        return $clone;
+    }
+
+    public function withoutState(): self
+    {
+        $clone = clone $this;
+        unset($clone->state);
+
+        return $clone;
+    }
+
+    /**
+     * Builds a new instance from an input array or object
+     *
+     * @param array|object $input Input data
+     * @param bool $validate If `false`, validation against the schema will be skipped.
+     * @return MyClass Created instance
+     * @throws \InvalidArgumentException
+     */
+    public static function fromInput(array|object $input, bool $validate = true): MyClass
+    {
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        if ($validate) {
+            static::validateInput($input);
+        }
+
+        $state = isset($input->{'state'}) ? $input->{'state'} : null;
+
+        $obj = new self($state);
+        return $obj;
+    }
+
+    /**
+     * Converts this object back to a simple array that can be JSON-serialized
+     *
+     * @return array Converted array
+     */
+    public function toArray(): array
+    {
+        $output = [];
+        if (isset($this->state)) {
+            $output['state'] = $this->state;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Converts this object to a stdClass that can be JSON-serialized
+     *
+     * @return \stdClass Converted object
+     */
+    public function toStdClass(): \stdClass
+    {
+        $output = new \stdClass();
+        if (isset($this->state)) {
+            $output->{'state'} = $this->state;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Validates the current instance against its schema
+     *
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result if `$return` is `true`
+     * @throws \InvalidArgumentException
+     */
+    public function validate(bool $return = false): bool
+    {
+        return self::validateInput($this->toStdClass(), $return);
+    }
+
+    /**
+     * Validates an input array
+     *
+     * @param array|object $input Input data
+     * @param bool $return Return instead of throwing errors
+     * @return bool Validation result if `$return` is `true`
+     * @throws \InvalidArgumentException
+     */
+    public static function validateInput(array|object $input, bool $return = false): bool
+    {
+        $validator = new \JsonSchema\Validator();
+        $input = is_array($input) ? \JsonSchema\Validator::arrayToObjectRecursive($input) : $input;
+        $validator->validate($input, self::$_schema);
+
+        if (!$validator->isValid() && !$return) {
+            $errors = array_map(function(array $e): string {
+                return ($e["property"] ? $e["property"] . ": " : "") . $e["message"];
+            }, $validator->getErrors());
+            throw new \InvalidArgumentException(join(".\n", $errors));
+        }
+
+        return $validator->isValid();
+    }
+}
